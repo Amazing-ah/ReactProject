c1d5c886518ea533e1b48f4a3a1f1fb72d250425
wang teng <wt1430@163.com>
2020-09-14 19:08:42 +0800

    9-10 react 分类bug修改

--- src/pages/listDetail/listDetail.js
+++ src/pages/listDetail/listDetail.js
@@ -1,54 +1,49 @@
-import React, { Component } from 'react'
-import './listDetail.css'
-import List from './components/list'
-import Header from '../../components/header/Header'
-import { reqGetGoods } from '../../utils/request'
-/* 
-FIXME
-返回直接返回到第一个
-cate。js
- */
+import React, { Component } from "react";
+import "./listDetail.css";
+import List from "./components/list";
+import Header from "../../components/header/Header";
+import { reqGetGoods } from "../../utils/request";
 export default class ListDetail extends Component {
-    constructor() {
-        super()
-        this.state = {
-            name: '',
-            list: []
-        }
-    }
-    componentDidMount() {
-        console.log(this.props);
-        reqGetGoods({ fid: this.props.match.params.id + '' }).then(res => {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      list: [],
+    };
+  }
+  componentDidMount() {
+    console.log(this.props);
+    reqGetGoods({ fid: this.props.match.params.id + "" }).then((res) => {
+      this.setState({
+        list: res.data.list,
+        name: this.props.match.params.name,
+      });
+    });
+  }
 
-            this.setState({
-                list: res.data.list,
-                name: this.props.match.params.name
-            })
-        })
-    }
+  // 子组件方法
+  goDetail(id) {
+    // 跳转到商品详情
+    this.props.history.push("/detail/" + id);
+  }
+  button() {
+    this.props.history.push("/login");
+  }
+  render() {
+    const { list, name } = this.state;
 
-    // 子组件方法
-    goDetail(id) {
-        // 跳转到商品详情
-        this.props.history.push('/detail/' + id)
-    }
-    button() {
-        this.props.history.push('/login')
-    }
-    render() {
-        console.log(this.props);
-        const { list, name } = this.state
-
-        return (
-
-            <div>
-                <Header title={name} back></Header>
-                {
-                    list.length > 0 ? <List info={list} goDetail={(id) => { this.goDetail(id) }}></List> : null
-                }
-
-
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Header title={name} back></Header>
+        {list !== null ? (
+          <List
+            info={list}
+            goDetail={(id) => {
+              this.goDetail(id);
+            }}
+          ></List>
+        ) : null}
+      </div>
+    );
+  }
 }
